e6c397af58bb77c1f1b16f364136ea43be000e08
NataBash <2864tasha@gmail.com>
2024-12-30 23:23:07 +0100

    Подключает Prettier к проекту

--- add-comments.js
+++ add-comments.js
@@ -4,27 +4,26 @@ const inputName = document.querySelector(".add-form-name");
 const inputComment = document.querySelector(".add-form-text");
 const button = document.querySelector(".add-form-button");
 
+export function addComments(comments) {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-export function addComments (comments) {
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-      
-        if (inputName.value.trim() === "" || inputComment.value.trim() === "") {
-          alert("Пожалуйста, заполните все поля.");
-          return;
-        }
-      
-        const currentDateTime = new Date().toLocaleString();
-        comments.push({
-          name: inputName.value,
-          date: currentDateTime,
-          text: inputComment.value,
-          likes: 0,
-          isLiked: false,
-        });
-      
-        inputName.value = "";
-        inputComment.value = "";
-        renderComments();
-      });
+    if (inputName.value.trim() === "" || inputComment.value.trim() === "") {
+      alert("Пожалуйста, заполните все поля.");
+      return;
+    }
+
+    const currentDateTime = new Date().toLocaleString();
+    comments.push({
+      name: inputName.value,
+      date: currentDateTime,
+      text: inputComment.value,
+      likes: 0,
+      isLiked: false,
+    });
+
+    inputName.value = "";
+    inputComment.value = "";
+    renderComments();
+  });
 }
