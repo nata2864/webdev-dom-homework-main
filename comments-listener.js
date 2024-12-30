5d5dbda9d560279b22ee6a7b45f8caf91c40498c
NataBash <2864tasha@gmail.com>
2024-12-30 23:13:12 +0100

    Разделяет код на модули

---
+++ comments-listener.js
@@ -0,0 +1,38 @@
+
+export function initComments(comments, escapeHTML) {
+    const commentsList = document.querySelector(".comments");
+    if (!commentsList) {
+      throw new Error("Element '.comments' not found");
+    }
+  
+    const inputName = document.querySelector(".add-form-name");
+    const inputComment = document.querySelector(".add-form-text");
+  
+    commentsList.addEventListener("click", (event) => {
+      const target = event.target;
+  
+      if (target.classList.contains("like-button")) {
+        const index = target.dataset.index;
+        const comment = comments[index];
+  
+        comment.isLiked = !comment.isLiked;
+        comment.likes += comment.isLiked ? 1 : -1;
+  
+        const commentElement = target.closest(".comment");
+        const likesCounter = commentElement.querySelector(".likes-counter");
+        likesCounter.textContent = comment.likes;
+        target.classList.toggle("-active-like", comment.isLiked);
+        return;
+      }
+  
+      const commentElement = target.closest(".comment");
+      if (commentElement) {
+        const index = commentElement.dataset.index;
+        const comment = comments[index];
+  
+        inputComment.value = `> ${escapeHTML(comment.text)}\n\n`;
+        inputName.value = escapeHTML(comment.name);
+      }
+    });
+  }
+  
---
+++ render-comments.js
@@ -0,0 +1,33 @@
+import { comments } from "./mocks.js";
+import { escapeHTML } from "./utils.js";
+
+const commentsList = document.querySelector(".comments");
+
+export const renderComments = () => {
+  commentsList.innerHTML = "";
+  comments.forEach((comment, index) => {
+    const className = comment.isLiked
+      ? "like-button -active-like"
+      : "like-button";
+    const commentHTML = `
+        <li class="comment" data-index="${index}">
+          <div class="comment-header">
+            <div>${escapeHTML(comment.name)}</div>
+            <div>${escapeHTML(comment.date)}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">
+              ${escapeHTML(comment.text)}
+            </div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likes}</span>
+              <button class="${className}" data-index="${index}"></button>
+            </div>
+          </div>
+        </li>`;
+    commentsList.innerHTML += commentHTML
+    
+  });
+};
