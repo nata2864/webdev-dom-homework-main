fceb146a6e46e7da7330419f46ca2e11ffb87232
NataBash <2864tasha@gmail.com>
2025-01-12 20:40:41 +0100

    Подключает плагин Prettier в ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
